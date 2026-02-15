68b7e7d9a00a68ea61a82a51c8e8e0b70f8333e6
RockieRaheem <Kamwangaraheem2050@gmail.com>
2026-02-15 23:25:37 +0300

    Update index file for bank deposit slips feature

--- edupay-ledger/types/index.ts
+++ edupay-ledger/types/index.ts
@@ -10,4 +10,28 @@ export * from "./term-comparison";
 export * from "./bad-debt";
 export * from "./escalation";
 export * from "./whatsapp";
-export * from "./bank-deposit";
+// Bank deposit types - selective export to avoid conflicts with receipt-book
+export type {
+  UgandaBank,
+  DepositStatus,
+  BankDepositSlip,
+  CashDenominations as DepositCashDenominations,
+  ChequeDetail,
+  SchoolBankAccount,
+  SlipSettings,
+  DepositBatch,
+  DepositReconciliation,
+  DepositSlipQuery,
+  DepositSummary,
+  DailyCashCollection,
+  DepositSlipPrintData,
+} from "./bank-deposit";
+export {
+  getBankInfo,
+  getDepositStatusInfo,
+  calculateDenominationTotal as calculateDepositDenominationTotal,
+  generateSlipNumber,
+  formatDenominationBreakdown as formatDepositDenominationBreakdown,
+  getEmptyDenominations as getEmptyDepositDenominations,
+  validateSlipForDeposit,
+} from "./bank-deposit";
